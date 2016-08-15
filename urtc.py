bec828dae2930d6cd6edbb0959f0c49cf9ffc6a4
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2016-08-15 12:25:20 +0200

    Fix typos

--- urtc.py
+++ urtc.py
@@ -7,7 +7,8 @@ except ImportError:
 
 
 DateTimeTuple = ucollections.namedtuple("DateTimeTuple",
-    "year,month,day,weekday,hour,minute,second,millisecond")
+    ["year", "month", "day", "weekday", "hour", "minute", "second",
+     "millisecond"])
 
 
 def datetime_tuple(year, month, day, weekday=0, hour=0, minute=0,
@@ -72,7 +73,8 @@ class BaseRTC:
         """
         buffer = bytearray(7)
         if datetime is None:
-            self.i2c.redfrom_mem_into(self, self._DATETIME_REGISTER, buffer)
+            self.i2c.readfrom_mem_into(self.address, self._DATETIME_REGISTER,
+                                       buffer)
             return datetime_tuple(
                 year=bcd2bin(buffer[6]) + 2000,
                 month=bcd2bin(buffer[5]),
@@ -102,33 +104,6 @@ class DS1307(BaseRTC):
         """Return ``True`` if and only if the clock is running."""
         return bool(self._register(0x00) & 0b10000000)
 
-    def pin_frequency(self, value=None):
-        """
-        Get or set the square wave pin frequency in hertz. Set to -1 to
-        switch the pin off. Available values: 0, 1, 4000, 8000 and 32000 Hz.
-        """
-        if value is None:
-            return {
-                0x00: 0,
-                0x80: -1,
-                0x10: 1,
-                0x11: 4000,
-                0x12: 8000,
-                0x13: 32000,
-            }[self._register(self._SQUARE_WAVE_REGISTER) & 0x93]
-        try:
-            buffer = bytearray(({
-                0: 0x00,
-                -1: 0x80,
-                1: 0x10,
-                4000: 0x11,
-                8000: 0x12,
-                32000: 0x13,
-            }[value],))
-        except KeyError:
-            raise ValueError("only 0, 1Hz, 4kHz, 8kHz and 32kHz are supported")
-        self._register(self._SQUARE_WAVE_REGISTER, buffer)
-
     def memory(self, address, buffer=None):
         """Read or write the non-volatile random access memory."""
         return self._register(self._NVRAM_REGISTER + address, buffer)
@@ -143,36 +118,11 @@ class DS3231(BaseRTC):
         """Return ``True`` if the clock lost power and needs to be set."""
         return bool(self._register(self._STATUS_REGISTER) & 0b10000000)
 
-    def datetime(self, datetime):
+    def datetime(self, datetime=None):
         if datetime is not None:
             status = self._register(self._STATUS_REGISTER) & 0b01111111
             self._register(self._STATUS_REGISTER, bytearray((status,)))
         return super().datetime(datetime)
-
-    def pin_frequency(self, value=None):
-        """
-        Get or set the square wave pin frequency. Available values are:
-        1, 1000, 4000 and 8000 Hz. 0 switches it off.
-        """
-        if value is None:
-            return {
-                0x01: 0,
-                0x00: 1,
-                0x08: 1000,
-                0x10: 4000,
-                0x18: 8000,
-            }[self._register(self._SQUARE_WAVE_REGISTER) & 0x93]
-        try:
-            buffer = bytearray(({
-                0: 0x01,
-                1: 0x00,
-                1000: 0x08,
-                4000: 0x10,
-                8000: 0x18,
-            }[value],))
-        except KeyError:
-            raise ValueError("only 1Hz, 1kHz, 4kHz and 8kHz are supported")
-        self._register(self._SQUARE_WAVE_REGISTER, buffer)
 
 
 class PCF8523(BaseRTC):
@@ -184,7 +134,7 @@ class PCF8523(BaseRTC):
     _SQUARE_WAVE_REGISTER = 0x0f
 
     def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.init()
 
     def init(self):
@@ -196,7 +146,7 @@ class PCF8523(BaseRTC):
         self._flag(self._CONTROL1_REGISTER, 0x58, True)
         self.init()
 
-    def lost_power(self, value):
+    def lost_power(self, value=None):
         """Get or set the power lost flag."""
         return self._flag(self._CONTROL3_REGISTER, 0b00010000, value)
 
@@ -204,51 +154,14 @@ class PCF8523(BaseRTC):
         """Get or set the stopped clock status."""
         return self._flag(self._CONTROL1_REGISTER, 0b00010000, value)
 
-# We always want 24-hour mode.
-#    def hour12(self, value):
-#        """Get or set the 12-hour mode."""
-#        return self._flag(self._CONTROL1_REGISTER, 0b00001000, value)
-
     def battery_low(self):
         """Returns ``True`` if the battery is low and needs replacing."""
         return self._flag(self._CONTROL3_REGISTER, 0b00000100)
 
-    def datetime(self, datetime):
+    def datetime(self, datetime=None):
         if datetime is not None:
             self.lost_power(False) # clear the battery switchover flag
         return super().datetime(datetime)
-
-    def pin_frequency(self, value=None):
-        """
-        Get or set the square wave pin frequency. Available values are:
-        1, 32, 1000, 4000, 8000, 16000 and 32000 Hz. 0 switches it off.
-        """
-        if value is None:
-            return {
-                7: 0,
-                6: 1,
-                5: 32,
-                4: 1000,
-                3: 4000,
-                2: 8000,
-                1: 16000,
-                0: 32000,
-            }[(self._register(self._SQUARE_WAVE_REGISTER) >> 3) & 0x07]
-        try:
-            buffer = bytearray(({
-                0: 7,
-                1: 6,
-                32: 5,
-                1000: 4,
-                4000: 3,
-                8000: 2,
-                16000: 1,
-                32000: 0,
-            }[value << 3],))
-        except KeyError:
-            raise ValueError("only 1Hz, 32Hz, 1kHz, 4kHz, 8kHz, 16kHz "
-                             "and 32kHz are supported")
-        self._register(self._SQUARE_WAVE_REGISTER, buffer)
 
     def alarm(self, value=None):
         """Get or set the status of alarm."""
@@ -266,7 +179,8 @@ class PCF8523(BaseRTC):
 
         buffer = bytearray(4)
         if datetime is None:
-            self.i2c.redfrom_mem_into(self, self._ALARM_REGISTER, buffer)
+            self.i2c.redfrom_mem_into(self.address, self._ALARM_REGISTER,
+                                      buffer)
             return datetime_tuple(
                 weekday=bcd2bin(buffer[3] & 0x7f) if buffer[0] & 0x80 else None,
                 day=bcd2bin(buffer[2] & 0x7f) if buffer[0] & 0x80 else None,
